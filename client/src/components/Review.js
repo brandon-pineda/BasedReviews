6a3c71afd2676652482005cfa3e271842c469613
Brandon Pineda <bppsparton@gmail.com>
2016-12-18 16:49:17 -0800

    initial commit

---
+++ client/src/components/Review.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export default class Review extends Component {
+
+    render() {
+        return (
+            <section className="review-container">
+                <p>this is the review</p>
+            </section>
+        );
+    }
+}
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/index.min.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
